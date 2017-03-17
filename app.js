bf2bae46c489ad221049bb3f459b6f7d15e43a72
Aakaash Jois <aakaashjois@gmail.com>
2017-03-17 13:38:16 +0530

    Added Detail View
    created a detail layout for the books

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ var states = {
     source: "https://www.googleapis.com/books/v1/volumes",
 }
 
+$(function () {
+    $('.search-form').submit(function (event) {
+        event.preventDefault();
+        getQuery();
+    });
+});
+
 function getQuery() {
     states.searchQuery = $('[type="text"]').val();
     states.startIndex = 0;
     states.maxResults = 40;
-    $('.js-books').empty();
+    $('.books').empty();
     getDatafromApi();
 }
 
@@ -16,16 +23,41 @@ function getDatafromApi() {
         startIndex: states.startIndex,
         maxResults: states.maxResults,
     }
-    $.getJSON(states.source, query, extractData);
-}
-
-function extractData(response) {
-    states.response = response;
-    items = response.items;
-    items.forEach(function(item, index){
-        $('.js-books').append(
-            "<div class='book'>" +
-            "<img src='"+item.volumeInfo.imageLinks.thumbnail+"'>" + 
-            "<label>" + item.volumeInfo.title+"</label>");
+    $.getJSON(states.source, query, function (response) {
+        if (response != undefined) {
+            states.items = response.items;
+            createOverview(states.items);
+        }
     });
 }
+
+function createOverview(items) {
+    items.forEach(function (item, index) {
+        $('.books').append(
+            "<div class='book' id=" + index + ">" +
+            "<img src='" + item.volumeInfo.imageLinks.thumbnail + "'>" +
+            "<p>" + item.volumeInfo.title + "</p>" +
+            "</div>");
+    });
+    $('.book').click(function () {
+        createDetailContent(this.id);
+        $('.book-detail').show();
+    });
+}
+
+function createDetailContent(index) {
+    item = states.items[index];
+    $('.close').click(function () {
+        $('.book-detail').hide();
+    });
+    $('.book-cover').attr('src', item.volumeInfo.imageLinks.thumbnail);
+    $('.book-title').html(item.volumeInfo.title);
+    $('.book-subtitle').html(item.volumeInfo.subtitle);
+    $('.book-author').html(item.volumeInfo.authors[0]);
+    $('.book-description').html(item.volumeInfo.description);
+    $('.book-buy').attr('href', item.saleInfo.buyLink);
+}
+
+//TODO: Show books below header
+//TODO: Create empty state when no books are found
+//TODO: Allow user to exit detail state by clicking outside the box
