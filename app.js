5edbfe0776ba52aa90f94b4b178f99935f1e1c53
Aakaash Jois <aakaashjois@gmail.com>
2017-03-17 14:18:23 +0530

    Added Pagination
    The site should load more data once the user scrolls to the end.

--- app.js
+++ app.js
@@ -1,44 +1,65 @@
 var states = {
-    source: "https://www.googleapis.com/books/v1/volumes",
 }
 
 $(function () {
     $('.search-form').submit(function (event) {
         event.preventDefault();
+        console.log("Getting query");
         getQuery();
     });
+    window.onscroll = function (event) {
+        if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight)
+            getNextPage();
+    }
 });
 
 function getQuery() {
     states.searchQuery = $('[type="text"]').val();
     states.startIndex = 0;
-    states.maxResults = 40;
+    console.log("Start Index:" + states.startIndex);
     $('.books').empty();
     getDatafromApi();
 }
 
+function getNextPage() {
+    console.log("Get Next Page");
+    states.startIndex = states.startIndex + states.items.length;
+    console.log("Next Start index: " + states.startIndex);
+    getDatafromApi();
+}
+
 function getDatafromApi() {
-    var query = {
+    console.log("Getting data from API");
+    $.getJSON("https://www.googleapis.com/books/v1/volumes", {
         q: states.searchQuery,
         startIndex: states.startIndex,
-        maxResults: states.maxResults,
-    }
-    $.getJSON(states.source, query, function (response) {
+        maxResults: 40,
+    }, function (response) {
         if (response != undefined) {
-            states.items = response.items;
-            createOverview(states.items);
+            if (states.items == undefined) {
+                states.items = response.items;
+                console.log("Created new state: " + states.items.length);
+            }
+            else {
+                states.items = states.items.concat(response.items);
+                console.log("Append state: " + states.items.length);
+            }
+            createOverview();
         }
     });
 }
 
-function createOverview(items) {
-    items.forEach(function (item, index) {
+function createOverview() {
+    console.log("Creating Overview");
+    for (var i = states.startIndex; i < states.items.length; i++) {
+        item = states.items[i];
+        console.log("index:" + i);
         $('.books').append(
-            "<div class='book' id=" + index + ">" +
+            "<div class='book' id=" + i + ">" +
             "<img src='" + item.volumeInfo.imageLinks.thumbnail + "'>" +
             "<p>" + item.volumeInfo.title + "</p>" +
             "</div>");
-    });
+    }
     $('.book').click(function () {
         createDetailContent(this.id);
         $('.book-detail').show();
